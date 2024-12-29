5b0aeb1225478862e22b87f752b2b389d957a115
amitt <amitt@192.168.1.48>
2024-12-29 13:11:43 +0530

    Added changes for database

--- src/main/java/com/amit/JpaDemo/App.java
+++ src/main/java/com/amit/JpaDemo/App.java
@@ -6,33 +6,35 @@ import javax.persistence.Persistence;
 
 public class App {
 
-  public static void main(String[] args) {
-  Employee employee = new App().getEmployeeDetails(3);
-  System.out.println(employee);
-//  new App().setEmployeeDetails(7, "Ashish", 27, "Enginnering");
-//  Employee newEmployee = new App().getEmployeeDetails(7);
-//  System.out.println(newEmployee);
+	public static void main(String[] args) {
+		//To get Employee Details from Table
+		//Employee employee = new App().getEmployeeDetails(103);
+		//System.out.println(employee);
 
-  }
-  
-  public Employee getEmployeeDetails(int employeeId) {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
-    EntityManager em = emf.createEntityManager();
-    Employee emp = em.find(Employee.class, employeeId);
-    return emp;
-  }
-  
-  public void setEmployeeDetails(int empId, String name, int age, String department) {
-    Employee empNew = new Employee();
-    empNew.setEmpID(empId);
-    empNew.setEmpName(name);
-    empNew.setEmpAge(age);
-    empNew.setEmpDept(department);
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
-    EntityManager em = emf.createEntityManager();
-    em.getTransaction().begin();
-    em.persist(empNew);
-    em.getTransaction().commit();
-  }
+		//To set Employee Details in Table
+		new App().setEmployeeDetails(107, "Aman", 27, "Engineering");
+		Employee newEmployee = new App().getEmployeeDetails(107);
+		System.out.println(newEmployee);
+	}
+
+	public Employee getEmployeeDetails(int employeeId) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+		EntityManager em = emf.createEntityManager();
+		Employee emp = em.find(Employee.class, employeeId);
+		return emp;
+	}
+
+	public void setEmployeeDetails(int empId, String name, int age, String department) {
+		Employee empNew = new Employee();
+		empNew.setEmpID(empId);
+		empNew.setEmpName(name);
+		empNew.setEmpAge(age);
+		empNew.setEmpDept(department);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(empNew);
+		em.getTransaction().commit();
+	}
 
 }
